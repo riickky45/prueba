220cbf52dafd7b4d613cd21bd16d0420fcb2ca29
riickky45 <riickky45@icloud.com>
2014-08-02 20:59:00 -0500

    Se crea un proyecto para modelar datos de  Forma

---
+++ ModeloDatosConcursante/ModeloDatosConcursante/ModeloForma.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatosConcursante
+{
+    class ModeloForma
+    {
+
+
+        public ModeloForma()
+        {
+
+        }
+
+        public Participante recuperaP(int id)
+        {
+            Participante par = new Participante();
+            //codigo que conecta y query para recuperar participante por id
+
+            return par;
+        }
+
+        public void crearP(Participante p)
+        {
+            //codigo que se conecta y query de insercion
+        }
+    }
+}
---
+++ ModeloDatosConcursante/ModeloDatosConcursante/Participante.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatosConcursante
+{
+    class Participante
+    {
+        public string nombre_participante;
+        public string id_tag;
+        public int id;
+    }
+}
